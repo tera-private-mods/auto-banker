b09fcabf50bb61d8f8f60d45f7c5c9dc8fb8b860
Dmitry S <dmitry.s@hsdn.org>
2022-05-06 05:12:13 +0400

    Update config_generator.js

--- config_generator.js
+++ config_generator.js
@@ -1,33 +1,35 @@
-'use strict'
+/* eslint-disable no-param-reassign */
+"use strict";
 
 const DefaultSettings = {
-  blacklist: {},
-  delay: 150,
-  enabled: true
-}
+	"blacklist": {},
+	"delay": 150,
+	"enabled": true
+};
 
 module.exports = function MigrateSettings(from_ver, to_ver, settings) {
-  if (from_ver === undefined) {
-    return Object.assign(Object.assign({}, DefaultSettings), settings)
-  } else if (from_ver === null) {
-    return DefaultSettings
-  } else {
-    if (from_ver + 1 < to_ver) {
-      settings = MigrateSettings(from_ver, from_ver + 1, settings)
-      return MigrateSettings(from_ver + 1, to_ver, settings)
-    }
-    switch (to_ver) {
-      default:
-        let oldsettings = settings
+	if (from_ver === undefined) {
+		return { ...DefaultSettings, ...settings };
+	} else if (from_ver === null) {
+		return DefaultSettings;
+	} else {
+		if (from_ver + 1 < to_ver) {
+			settings = MigrateSettings(from_ver, from_ver + 1, settings);
+			return MigrateSettings(from_ver + 1, to_ver, settings);
+		}
+		switch (to_ver) {
+			default:
+				// eslint-disable-next-line no-case-declarations
+				const oldsettings = settings;
+				settings = Object.assign(DefaultSettings, {});
+				for (const option in oldsettings) {
+					if (settings[option] !== undefined) {
+						settings[option] = oldsettings[option];
+					}
+				}
+				break;
+		}
 
-        settings = Object.assign(DefaultSettings, {})
-
-        for (let option in oldsettings) {
-          if (settings[option]) { settings[option] = oldsettings[option] }
-        }
-        break
-    }
-
-    return settings
-  }
-}
+		return settings;
+	}
+};
